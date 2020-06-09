53337042cdef99acad524d5074b80eb71f692c43
SongYih90 <43133514+SongYih90@users.noreply.github.com>
2020-06-10 00:06:15 +0800

    Missing commit

--- DatabaseScript.sql
+++ DatabaseScript.sql
@@ -1,39 +1,94 @@
-
-select * from dbo.employees WHERE (LoginName like '%s%')
-
-
-UPDATE Employees SET LoginName = '', name = '', Salary = '' WHERE EmployeeID = ''
-
-INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
-VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
-
-SELECT * FROM Employees WHERE EmployeeID = 'e0001'
-
-ALTER TABLE Employees
-ADD CONSTRAINT <CONSTRAINT_NAME> PRIMARY KEY(<COLUMN_NAME>)
-
-e0001	hpotter
-e0002	rwesley
-e0003	ssnape
-e0004	rhagrid
-e0005	voldemort
-e0006	gwesley
-e0007	hgranger
-e0008	adumbledore
-e0009	dmalfoy
-e0010	basilisk
-e0011	sy
-
-e0001	hpotter	Harry Potter	1235.00
-e0002	rwesley	Ron Weasley	19234.50
-e0003	ssnape	Severus Snape	4000.00
-e0004	rhagrid	Rubeus Hagrid	4000.00
-e0005	voldemort	Lord Voldemort	523.40
-e0006	gwesley	Ginny Weasley	4000.00
-e0007	hgranger	Hermione Granger	0.00
-e0008	adumbledore	Albus Dumbledore	34.23
-e0009	dmalfoy	Draco Malfoy	34234.50
-e0010	basilisk	Basilisk	-23.43
-e0011	sy	SYL	9898989.99
-
-drop table Employees
+USE [master]
+GO
+/****** Object:  Database [SalaryManagement]    Script Date: 29/5/2020 1:22:48 AM ******/
+CREATE DATABASE [SalaryManagement] ON  PRIMARY 
+( NAME = N'SalaryManagement', FILENAME = N'C:\Program Files (x86)\Microsoft SQL Server\MSSQL.1\MSSQL\DATA\SalaryManagement.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'SalaryManagement_log', FILENAME = N'C:\Program Files (x86)\Microsoft SQL Server\MSSQL.1\MSSQL\DATA\SalaryManagement_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+ALTER DATABASE [SalaryManagement] SET COMPATIBILITY_LEVEL = 90
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [SalaryManagement].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [SalaryManagement] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [SalaryManagement] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [SalaryManagement] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [SalaryManagement] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [SalaryManagement] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [SalaryManagement] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [SalaryManagement] SET  MULTI_USER 
+GO
+ALTER DATABASE [SalaryManagement] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [SalaryManagement] SET DB_CHAINING OFF 
+GO
+USE [SalaryManagement]
+GO
+/****** Object:  Table [dbo].[Employees]    Script Date: 29/5/2020 1:22:49 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Employees](
+	[EmployeeID] [nvarchar](50) NOT NULL,
+	[LoginName] [nvarchar](50) NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+	[Salary] [decimal](18, 2) NOT NULL,
+ CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED 
+(
+	[EmployeeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+INSERT [dbo].[Employees] ([EmployeeID], [LoginName], [Name], [Salary]) VALUES (N'e0001', N'hpotter', N'Harry Potter', CAST(1235.01 AS Decimal(18, 2)))
+INSERT [dbo].[Employees] ([EmployeeID], [LoginName], [Name], [Salary]) VALUES (N'e0002', N'rwesley', N'Ron Weasley', CAST(19234.50 AS Decimal(18, 2)))
+INSERT [dbo].[Employees] ([EmployeeID], [LoginName], [Name], [Salary]) VALUES (N'e0003', N'ssnape', N'Severus Snape', CAST(4000.00 AS Decimal(18, 2)))
+INSERT [dbo].[Employees] ([EmployeeID], [LoginName], [Name], [Salary]) VALUES (N'e0004', N'rhagrid', N'Rubeus Hagrid', CAST(4000.00 AS Decimal(18, 2)))
+INSERT [dbo].[Employees] ([EmployeeID], [LoginName], [Name], [Salary]) VALUES (N'e0005', N'voldemort', N'Lord Voldemort', CAST(523.40 AS Decimal(18, 2)))
+INSERT [dbo].[Employees] ([EmployeeID], [LoginName], [Name], [Salary]) VALUES (N'e0006', N'gwesley', N'Ginny Weasley', CAST(4000.00 AS Decimal(18, 2)))
+INSERT [dbo].[Employees] ([EmployeeID], [LoginName], [Name], [Salary]) VALUES (N'e0007', N'hgranger', N'Hermione Granger', CAST(0.01 AS Decimal(18, 2)))
+USE [master]
+GO
+ALTER DATABASE [SalaryManagement] SET  READ_WRITE 
+GO
